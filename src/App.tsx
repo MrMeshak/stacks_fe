bed0746788eda0fca17b71b08aeffb5001f926c1
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-01-27 12:00:09 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import HomePage from "./app/homePage";
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app/homePage.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface IHomePageProps {}
+
+export default function HomePage(props: IHomePageProps) {
+  return (
+    <div>
+      <h3>Home Page</h3>
+    </div>
+  );
+}
