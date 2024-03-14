c9fe0981f96a07a2c0937439d962b722f22d3df3
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-03-14 11:16:41 +1100

    add homepage redirect to login page and sever wake query

--- src/app/homePage.tsx
+++ src/app/homePage.tsx
@@ -1,6 +1,24 @@
+import { User, httpClient } from '@/axios';
+import { useQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export interface IHomePageProps {}
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const meQuery = useQuery({
+    queryKey: ['users', 'me'],
+    queryFn: async () => {
+      return await httpClient.get<User>('/users/me');
+    },
+  });
+
+  useEffect(() => {
+    navigate('/login');
+  }, []);
+
   return (
     <div>
       <h3>Home Page</h3>
